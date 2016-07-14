7831560e5efe5ead6cd61d3cd3b3dde1d15081b5
Chris Freeman <cfreeman.code@gmail.com>
2016-07-14 11:14:44 -0500

    add acceptance test for let helper

---
+++ tests/acceptance/let-helper-test.js
@@ -0,0 +1,38 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | let helper');
+
+test ('let binds basic values', function(assert) {
+  visit('/');
+
+  andThen(() => {
+    assert.equal(find('ul.basic li:first').text(), 'abc');
+    assert.equal(find('ul.basic li:last').text(), '123');
+  });
+});
+
+test ('let binds nested values', function(assert) {
+  visit('/');
+
+  andThen(() => {
+    let expected = ['first', 'second', 'third'];
+    find('ul.with-hash li').each((i, v) => {
+        assert.equal($(v).text(), expected[i], `should equal ${expected[i]}`);
+      });
+  });
+});
+
+test ('let binds class instances', function(assert) {
+  visit('/');
+
+  andThen(() => {
+    assert.equal(find('.with-helper-object #bool-value').text(), 'false', 'bool.value should start as false');
+  });
+
+  click('button#toggle-bool');
+
+  andThen(() => {
+    assert.equal(find('.with-helper-object #bool-value').text(), 'true', 'bool.value should be false after the toggle method is called');
+  });
+});
