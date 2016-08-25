c3da9d564b03ad55e594948c2f2a6fb7b90980ac
Charles Lowell <cowboyd@frontside.io>
2016-08-25 17:12:01 -0500

    remove andThen helper
    BDD style encourages the explicit separation of actions and
    consequences. Actions are initiated in  the `beforeEach` blocks
    associated with the `describe` describing the action, and consequences
    are verified inside the `it` block.
    
    The general rule is that `it` should have no side-effects.

--- tests/acceptance/let-helper-test.js
+++ tests/acceptance/let-helper-test.js
@@ -24,41 +24,39 @@ describe('Acceptance: let helper', function() {
   });
 
   it('binds basic values', () => {
-    andThen(() => {
-      expect(find('ul.basic li:first').text()).to.equal('abc');
-      expect(find('ul.basic li:last').text()).to.equal('123');
-    });
+    expect(find('ul.basic li:first').text()).to.equal('abc');
+    expect(find('ul.basic li:last').text()).to.equal('123');
   });
 
   it('binds nested values', () => {
-    andThen(() => {
-      let expected = ['first', 'second', 'third'];
+    let expected = ['first', 'second', 'third'];
 
-      find('ul.with-hash li').each((i, v) => {
-        expect($(v).text()).to.equal(expected[i]);
-      });
+    find('ul.with-hash li').each((i, v) => {
+      expect($(v).text()).to.equal(expected[i]);
     });
   });
 
-  it('binds class instances', () => {
-    andThen(() => {
+  describe('binding css classes', ()=> {
+    it('starts out with one value', ()=> {
       expect(find('.with-helper-object #bool-value').text()).to.equal('false');
     });
 
-    click('button#toggle-bool');
+    describe('toggling the boolean', function() {
+      beforeEach(function() {
+        click('button#toggle-bool');
+      });
 
-    andThen(() => {
-      expect(find('.with-helper-object #bool-value').text()).to.equal('true');
+      it('changes the value', function() {
+        expect(find('.with-helper-object #bool-value').text()).to.equal('true');
+      });
     });
   });
 
   it('will yield its block even when the value is falsey', () => {
     let expected = ['this is undefined', 'this is null', 'this is an empty array'];
 
-    andThen(() => {
-      find('.missing-values li').each((i, v) => {
-        expect($(v).text()).to.equal(expected[i]);
-      });
+    find('.missing-values li').each((i, v) => {
+      expect($(v).text()).to.equal(expected[i]);
     });
   });
 
