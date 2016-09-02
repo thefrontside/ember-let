302b22400195f9352cf203bbf2937ae2bcb1c9e6
Robert Jackson <me@rwjblue.com>
2016-09-02 09:45:39 -0400

    Add glimmer support. (#18)

--- addon/initializers/register-let-helper.js
+++ addon/initializers/register-let-helper.js
@@ -1,18 +1,46 @@
 import Ember from 'ember';
 
-const { registerHelper } = Ember.__loader.require('ember-htmlbars/helpers');
+if (Ember.__loader.registry['ember-glimmer/syntax']) {
+  let { registerSyntax } = Ember.__loader.require('ember-glimmer');
+  let { StatementSyntax } = Ember.__loader.require('glimmer-runtime');
+
+  class LetSyntax extends StatementSyntax {
+    static create(environment, args, templates) {
+      return new this(args, templates);
+    }
+
+    constructor(args, templates) {
+      super();
+      this.args = args;
+      this.templates = templates;
+    }
+
+    compile(dsl) {
+      let { args, templates } = this;
+
+      dsl.block({ templates, args }, (dsl) => {
+        dsl.evaluate('default');
+      });
+    }
+  }
+
+  registerSyntax('let', LetSyntax);
+} else if (Ember.__loader.registry['ember-htmlbars/helpers']) {
+  let { registerHelper } = Ember.__loader.require('ember-htmlbars/helpers');
+
+  registerHelper('let', function letHelper(params, hash, options) {
+    let isBlock = !!options.template.yield;
+    // when used as a block
+    if (isBlock) {
+      options.template.yield(params);
+    }
+  });
+}
 
 export default {
   name: 'register-let-helper',
-  initialize: function registerLetHelper() {
-    registerHelper('let', function letHelper(params, hash, options) {
 
-      let isBlock = !!options.template.yield;
-      // when used as a block
-      if (isBlock) {
-        options.template.yield(params);
-      }
-
-    });
-  }
+  // registration is done once at module eval time
+  // no need to do anything per-application boot
+  initialize() { }
 };
